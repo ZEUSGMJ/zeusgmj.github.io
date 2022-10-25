0a907e16fcd7006468a8459ce3ee1eea4fd4bad3
ZEUSGMJ <underminertheone@gmail.com>
2022-10-26 00:39:24 +0530

    Updated stylings and content

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-const burger = document.querySelector('.burger')
-const nav = document.querySelector('.nav')
-const navUl = document.querySelector('.nav-items')
-const navItems = document.querySelectorAll('.nav-items li')
-const toggle = document.getElementById('toggle')
-const theme = localStorage.getItem('site-theme')
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
+const navUl = document.querySelector('.nav-items');
+const navItems = document.querySelectorAll('.nav-items li');
+const toggle = document.getElementById('toggle');
+const theme = localStorage.getItem('site-theme');
 
-window.addEventListener('scroll', fadeNav)
-navSlide()
+window.onscroll(fadeNav)
+
+navSlide();
 
 if (theme == 'lightmode') {
-    document.body.classList.add('lightmode')
-    toggle.checked = true
-} else toggle.checked = false
+    document.body.classList.add('lightmode');
+    toggle.checked = true;
+} else toggle.checked = false;
 
 toggle.addEventListener('input', (e) => {
     const isChecked = e.target.checked;
 
     if (isChecked) {
-        document.body.classList.add('lightmode')
-        setTheme('lightmode')
+        document.body.classList.add('lightmode');
+        setTheme('lightmode');
     }
     else {
-        document.body.classList.remove('lightmode')
-        setTheme(null)
+        document.body.classList.remove('lightmode');
+        setTheme(null);
     }
 });
 
@@ -31,38 +32,38 @@ function setTheme(themeName) {
 }
 
 function fadeNav() {
-    if (window.scrollY > nav.offsetHeight + 100) nav.classList.add('active')
-    else nav.classList.remove('active')
+    if (window.scrollY > nav.offsetHeight + 100) nav.classList.add('active');
+    else nav.classList.remove('active');
 }
 
 function navSlide() {
 
-    if(burger) {
+    if (burger) {
         burger.addEventListener('click', () => {
 
-            navUl.classList.toggle('nav-active')
-            if (navUl.classList.contains('nav-active')) document.body.style.position = 'fixed'
-            else document.body.style.position = 'static'
+            navUl.classList.toggle('nav-active');
+            if (navUl.classList.contains('nav-active')) document.body.style.position = 'fixed';
+            else document.body.style.position = 'static';
 
-            navItems.forEach( (item, idx) => {
-                item.addEventListener('click', closeNav)
+            navItems.forEach((item, idx) => {
+                item.addEventListener('click', closeNav);
                 if (item.style.animation) {
-                    item.style.animation = null
+                    item.style.animation = null;
                 } else {
-                    item.style.animation = `navItemsFadeIn 0.5s ease forwards ${idx / 7 + 0.3}s`
+                    item.style.animation = `navItemsFadeIn 0.5s ease forwards ${idx / 7 + 0.3}s`;
                 }
             });
-            burger.classList.toggle('toggle')
+            burger.classList.toggle('toggle');
         });
     }
 }
 
-function closeNav()  {
-    document.body.style.position = 'static'
-    navUl.classList.toggle('nav-active')
-    burger.classList.toggle('toggle')
+function closeNav() {
+    document.body.style.position = 'static';
+    navUl.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
 
     navItems.forEach(item => {
-        if (item.style.animation) item.style.animation = null
+        if (item.style.animation) item.style.animation = null;
     })
 }
