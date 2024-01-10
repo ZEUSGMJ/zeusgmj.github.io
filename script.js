8ef04e77eda286c7c836a31440397fc75a6dce44
ZEUSGMJ <underminertheone@gmail.com>
2024-01-11 05:27:20 +0530

    Added quotes.

--- script.js
+++ script.js
@@ -4,8 +4,55 @@ const navUl = document.querySelector('.nav-items');
 const navItems = document.querySelectorAll('.nav-items li');
 const toggle = document.getElementById('toggle');
 const theme = localStorage.getItem('site-theme');
+const quote = document.getElementById('quote');
+const quoteAUthor = document.getElementById('quote-author');
 
-window.addEventListener('scroll',fadeNav);
+// Quotes
+const quotes = [
+    {
+        quote: "What, So Everyone's Supposed To Sleep Every Single Night Now?",
+        author: 'Rick Sanchez'
+    },
+    {
+        quote: "Uncertainty is inherently unsustainable. Eventually, everything either is or isn't.",
+        author: 'Rick Sanchez'
+    },
+    {
+        quote: "When smart people get happy, they stop recognising themselves.",
+        quote: 'Rick Sanchez'
+    },
+    {
+        quote: "Why Should You Be Embarrassed About Being Honest And Saying Something Nice?",
+        author: 'Tony Johnson - After Life'
+    },
+    {
+        quote: "That's just the way it is. Change is inevitable. Instead of resisting it, you’re better served simply going with the flow.",
+        author: 'Shunsui Kyōraku - Bleach'
+    },
+    {
+        quote: "If a miracle only happens once, then what is it called the second time?",
+        author: 'Ichigo Kurosaki - Bleach'
+    },
+    {
+        quote: "Any betrayal you can see is trivial, what is truly frightening and much more lethal, is the betrayal you cannot see.",
+        author: 'Sosuke Aizen - Bleach'
+    },
+    {
+        quote: "If you can only do one a single thing, hone it to perfection. Hone it to the utmost limit!",
+        author: "Jigoro Kuwajima - Demon Slayer"
+    },
+    {
+        quote: "A goal is not always meant to be reached, it often serves simply as something to aim at.",
+        author: "Bruce Lee"
+    }
+];
+
+window.addEventListener('scroll', fadeNav);
+
+const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = randomQuote.quote;
+quoteAUthor.innerText = '- ' + randomQuote.author;
 
 navSlide();
 
